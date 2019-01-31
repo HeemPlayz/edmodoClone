5313a8233941e0dd4567fc3a281cc5ba2be8cdce
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-01-31 23:42:34 +0800

    added login and sign up

---
+++ core/init.php
@@ -0,0 +1,18 @@
+<?php
+
+    include 'database/connection.php';
+
+    include 'classes/user.php';
+    include 'classes/tweet.php';
+    include 'classes/follow.php';
+
+    global $pdo;
+
+    session_start();
+    
+    $getFromUserClass = new User($pdo);
+    $getFromTweetClass = new Tweet($pdo);
+    $getFromFollowClass = new User($pdo);
+
+    define("BASE_URL", "http://localhost/edmodoClone");
+?>
